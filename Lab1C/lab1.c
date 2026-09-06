94263c599fda4c83ce802196ab2e21817f817e10
Ibrohimjon Nematzhonov <nematjanovibrohimjon@gmail.com>
2026-09-06 17:46:18 +0500

    Implement array and Lab1C tasks

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -5,36 +5,97 @@ Array *array_create_and_read(FILE *input)
 {
     int n;
     fscanf(input, "%d", &n);
-    /* Create array */
+
     Array *arr = array_create(n, NULL);
-    /* Read array data */
-    for (int i = 0 ; i < n ; ++i)
+
+    for (int i = 0; i < n; ++i)
     {
         int x;
         fscanf(input, "%d", &x);
         array_set(arr, i, x);
     }
+
     return arr;
 }
 
 void task1(Array *arr)
 {
+    Array *positive = array_create(array_size(arr), NULL);
+    Array *negative = array_create(array_size(arr), NULL);
+
+    size_t positive_size = 0;
+    size_t negative_size = 0;
+
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        int x = (int)array_get(arr, i);
+
+        if (x > 0)
+        {
+            array_set(positive, positive_size, x);
+            ++positive_size;
+        }
+        else if (x < 0)
+        {
+            array_set(negative, negative_size, x);
+            ++negative_size;
+        }
+    }
+
+    for (size_t i = 0; i < positive_size; ++i)
+        printf("%d ", (int)array_get(positive, i));
+
+    printf("\n");
+
+    for (size_t i = 0; i < negative_size; ++i)
+        printf("%d ", (int)array_get(negative, i));
+
+    printf("\n");
+
+    array_delete(positive);
+    array_delete(negative);
 }
 
 void task2(Array *arr)
 {
+    int count[1001] = {0};
+
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        int x = (int)array_get(arr, i);
+        ++count[x];
+    }
+
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        int x = (int)array_get(arr, i);
+
+        if (count[x] == 1)
+            printf("%d ", x);
+    }
+
+    printf("\n");
 }
 
 int main(int argc, char **argv)
 {
-    Array *arr = NULL;
+    if (argc < 2)
+        return 1;
+
     FILE *input = fopen(argv[1], "r");
-    arr = array_create_and_read(input);
+
+    if (input == NULL)
+        return 1;
+
+    Array *arr = array_create_and_read(input);
     task1(arr);
     array_delete(arr);
-    /* Create another array here */
+
     arr = array_create_and_read(input);
     task2(arr);
     array_delete(arr);
+
     fclose(input);
+
+    return 0;
 }
