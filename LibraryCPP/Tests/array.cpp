976fe50d88b3d8d025700bba1d7c9ad974c335f1
Ibrohimjon Nematzhonov <nematjanovibrohimjon@gmail.com>
2026-09-11 17:48:57 +0500

    Add array tests

--- LibraryCPP/Tests/array.cpp
+++ LibraryCPP/Tests/array.cpp
@@ -1,29 +1,19 @@
-#include <iostream>
+#include <cassert>
 #include "array.h"
 
 int main()
 {
-    Array *arr = array_create(10);
+    Array* arr = array_create(10);
 
-    if (array_size(arr) != 10)
-    {
-        std::cout << "Invalid array size\n";
-        array_delete(arr);
-        return 1;
-    }
+    assert(array_size(arr) == 10);
 
-    for (int i = 0 ; i < 10 ; ++i)
+    for (int i = 0; i < 10; ++i)
         array_set(arr, i, i * 2);
 
-    for (int i = 0 ; i < 10 ; ++i)
-    {
-        if (array_get(arr, i) != i * 2)
-        {
-            std::cout << "Invalid array element " << i << "\n";
-            array_delete(arr);
-            return 1;
-        }
-    }
+    for (int i = 0; i < 10; ++i)
+        assert(array_get(arr, i) == i * 2);
 
     array_delete(arr);
+
+    return 0;
 }
